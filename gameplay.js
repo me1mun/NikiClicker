18269fd770c936ce5b78bdb3fddbb17b3d4d986d
Serhii <108861843+me1mun@users.noreply.github.com>
2024-03-23 03:42:59 +0200

    7

--- gameplay.js
+++ gameplay.js
@@ -128,15 +128,15 @@ function openModal(modalId) {
 
     setTimeout(function() {
         modal.style.backgroundColor = "rgba(0, 0, 0, 0.75)";
-        modal.getElementsByClassName('modal-content')[0].style.bottom = "0";
+        modal.getElementsByClassName('modal-container')[0].style.bottom = "0";
     }, 50);
 }
 
 function closeModal(modalId) {
     var modal = document.getElementById(modalId);
-    var modalContent = modal.getElementsByClassName('modal-content')[0];
+    var modalContent = modal.getElementsByClassName('modal-container')[0];
 
-    modalContent.style.bottom = "-80%";
+    modalContent.style.bottom = "-100%";
     modal.style.backgroundColor = "rgba(0, 0, 0, 0)";
 
     modalContent.addEventListener("transitionend", function() {
